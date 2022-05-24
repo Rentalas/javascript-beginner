0843b664fe477bf99c3886d53e3f10934704971f
Rentalas <lozrak@gmail.com>
2022-05-24 17:44:12 +0300

    lodash polyfil fill

--- javascript-beginner/lodash-polyfils/array-method/fill.ts
+++ javascript-beginner/lodash-polyfils/array-method/fill.ts
@@ -1,9 +1,6 @@
 export const fill = (array, value, start, end) => {
-  array.map((el, i) => {
-     if (i >= start && i < end)
-        el = value
-        console.log(el)
-        console.log()
-  })  
-  return array
+  return array.map((el, i) => i >= start && i < end
+    ? value
+    : el
+  );
 }
