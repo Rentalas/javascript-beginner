7681ce0ae3e286f4e5836b3ea8243d84b352a03e
Rentalas <lozrak@gmail.com>
2022-05-26 17:18:49 +0300

    little optimization

--- javascript-beginner/lodash-polyfils/array-method/flattenDepth.ts
+++ javascript-beginner/lodash-polyfils/array-method/flattenDepth.ts
@@ -29,13 +29,14 @@
 // }
 
 export const flattenDepth = (arr, depth = 1) => {
-  if (depth != 1) {
-   return arr.reduce((acc, el) => {
-const data = Array.isArray(el)
-    ? flattenDepth(el, depth - 1)
-    : el;
+    if (depth != 1) {
+        return arr.reduce((acc, el) => {
+            const data = Array.isArray(el)
+                ? flattenDepth(el, depth - 1)
+                : el;
 
-return acc.concat(data)}, []);
-}
+            return acc.concat(data)
+        }, []);
+    }
     return arr.reduce((acc1, el1) => acc1.concat(el1), [])
 };
